a17513dc7b1928328fccad577219748956106548
Najam <hmnajam@gmail.com>
2023-09-08 15:40:28 +0500

    commiting hero section that i mistakenly create in master before creating hero branch

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,15 @@
 import Image from "next/image";
 import Navbar from "./components/Navbar";
+import HeroSection from "./components/Hero";
 
 export default function Home() {
   return (
     <>
-    <Navbar />
-    {/* <Hero /> */}
-      <h1>Hello World</h1>
+      <Navbar />
+      <HeroSection />
+      <h1>Portfolio</h1>
+      <h1>Testemonials</h1>
+
     </>
-  )
+  );
 }
