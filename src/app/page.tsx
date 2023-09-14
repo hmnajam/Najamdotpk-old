17950967ecdbd1819c3d617356e348c531d93571
hmnajam <hmnajam@gmail.com>
2023-09-14 13:28:50 +0500

    created headings on front page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,38 +21,36 @@ export default function Home() {
         </div>
       </main> */}
 
-
-
-<main className="flex flex-wrap justify-center p-2">
-  <div className="w-full">
-    <h1 className="text-4xl font-bold text-center mt-10">Portfolio</h1>
-  </div>
-  <div className="w-full md:w-1/2 lg:w-1/2 xl:w-1/2 p-6">
+      <main className="flex flex-wrap justify-center p-2">
+        <div className="w-full">
+          <h1 className="text-4xl font-bold mt-10">Portfolio</h1>
+        </div>
+        <div className="w-full md:w-1/2 lg:w-1/2 xl:w-1/2 p-6">
+          <Portfoliocard />
+        </div>
+        <div className="w-full md:w-1/2 lg:w-1/2 xl:w-1/2 p-6">
+          <Portfoliocard />
+        </div>
+        {/* <div className="w-full md:w-1/2 lg:w-1/2 xl:w-1/2 p-6">
     <Portfoliocard />
   </div>
-  <div className="w-full md:w-1/2 lg:w-1/2 xl:w-1/2 p-6">
+  <div className="w-full md:w-1/2 lg:w-1/2 xl:w-1/
+  2 p-6">
     <Portfoliocard />
-  </div>
-  <div className="w-full md:w-1/2 lg:w-1/2 xl:w-1/2 p-6">
-    <Portfoliocard />
-  </div>
-  <div className="w-full md:w-1/2 lg:w-1/2 xl:w-1/2 p-6">
-    <Portfoliocard />
-  </div>
+  </div> */}
 
-</main>
+        <div className="w-full">
+          <h1 className="text-4xl font-bold mt-10">Testemonials</h1>
+        </div>
 
+        <div className="w-full">
+          <h1 className="text-4xl font-bold mt-10">Blog</h1>
+        </div>
 
-      <h1>Testemonials</h1>
-
-
-
-      <h1>Blog</h1>
-
-
-
-
-      <h1>About me</h1>
+        <div className="w-full">
+          <h1 className="text-4xl font-bold mt-10">About me</h1>
+        </div>
+      </main>
 
       <LargeWithNewsletter />
     </>
